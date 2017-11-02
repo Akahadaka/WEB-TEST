2cd3a43a8950c442a5db9d996e894c47192a6568
andrew.nash <akahadaka@gmail.com>
2017-11-02 15:12:55 +0200

    Install Material and Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+// angular
+import { BrowserModule }    from '@angular/platform-browser';
+import { NgModule }         from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule }     from '@angular/router';
 
+// app
+import { MaterialModule } from './shared/material/material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FlexLayoutModule,
+        MaterialModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
